055a0662286d107710138cc39d70cbe395444cd7
Eloisa-E <65208993+Eloisa-E@users.noreply.github.com>
2020-11-18 23:04:32 -0600

    anaisish3

--- resources/views/analisis/show1.blade.php
+++ resources/views/analisis/show1.blade.php
@@ -1,35 +1,39 @@
 @extends('template.plantilla2')
 @section('content')
 <?php 
-
+$valor1='2020-10-31';
+$valor2='2020-11-30';
 $conexion=mysqli_connect('localhost','root','','sistema_academico');
 
 ?>
 
 <h1>Analisis Horizontal</h1>
+<h2>{{$val1}}</h2>
+<h3>xx</h3>
 <table class="table table-bordered thead-dark table-hover table-sm" id="demo">
 
 
  <tr>
  <th>Cuenta</th>
- <th>Valor Anterior</th>
- <th>Valor Actual</th>
- <th>Variacion Absoluta</th>
- <th>Variacion relativa</th>
+ <th>{{$valor1}}</th>
+ <th>{{$valor2}}</th>
+ <th>Variación Absoluta</th>
+ <th>Variación relativa</th>
 </tr>
  </thead>
 <tbody>
 
 
 <?php
-$valor1='2020-10-01';
-$valor2='2020-11-30';
- $sql = "SELECT c.nombre AS nom, c.monto AS valor_Actual, b.monto As valor_anterior, c.monto-b.monto AS variacion From balances c, balances b WHERE c.fecha_final='$valor2' AND b.fecha_inicio='$valor1' AND c.nombre=b.nombre";
+
+ $sql = "SELECT c.nombre AS nom, c.monto AS valor_Actual, b.monto As valor_anterior, c.monto-b.monto AS variacion From balances c, balances b WHERE c.fecha_final='$valor2' AND b.fecha_final='$valor1' AND c.nombre=b.nombre";
 $result=mysqli_query($conexion,$sql);
 while($mostrar=mysqli_fetch_array($result)){
 $a=$mostrar['valor_anterior'];
 $b=$mostrar['variacion'];
-
+$c=$b/$a;
+$d=$c*100;
+$e=round($d,2);
 ?>
 
 <tr>
@@ -37,8 +41,21 @@ $b=$mostrar['variacion'];
 <td><?php echo $mostrar['valor_anterior']?></td>
 <td><?php echo $mostrar['valor_Actual']?></td>
 <td><?php echo $mostrar['variacion']?></td>
+<td><?php echo $e?>%</td>
+
+<?php
+if($mostrar['nom']=='ACTIVO'){
+$act= $e;
+}
+if($mostrar['nom']=='PASIVO'){
+      $pas= $e;
+      }
+if($mostrar['nom']=='PATRIMONIO'){
+      $pat= $e;
+      }
 
 
+?>
 
 </tr>
 
@@ -49,11 +66,62 @@ $b=$mostrar['variacion'];
 </tbody>
 
     
-      </table>
+</table>
 <h2>Resultados</h2>
 <table width="100%" border="0" cellspacing="0" cellpadding="4">
 <tr bgcolor="#FFFFFF">
-<td>Ponga aqui el contenido del cuadro</td>
+<?php
+if($act>0){
+?>
+<td><?php echo "La empresa logro un crecimiento  de $act de un período a otro, lo cual puede indicar que hubo aumento en las ventas o que los cambios realizados para la ejeucion de estas fueron acertivos.";
+?></td>
+<?php
+}elseif($act==0){
+?>
+<td><?php echo "La empresa no tuvo ningún crecimiento a lo largo del período, por lo que se recomienda evaluar las estrategias actuales de ventas.";
+?></td>
+<?php
+}else{
+?>
+<td><?php echo "La disminución del activo de la empresa en un $act%,implica que la empresa esta teniendo pérdidas duarante su ejercicio, por lo que se recomienda que revalue sus politicas de ventas y cobranzas para el siguiente periodo";
+?></td>
+<?php
+}
+?>
+</tr>
+<tr bgcolor="#FFFFFF">
+<?php
+if($pas>0){
+?>
+<td><?php echo "El aumento de $pas% en los  pasivo indica que la empresa se encuentra en una buena posición económica y que  puede disponer de credito adicional , por parte de los acreedores.
+";
+?></td>
+<?php
+}elseif($pas<0){
+?>
+<td><?php echo "La empresa decreció un $pas% de un período a otro, la disminución en los pasivos implica que se estan utilizando fondos para amortizar o cancelar cuentas de los acreedores.
+";
+?></td>
+<?php
+}
+?>
+</tr>
+<tr bgcolor="#FFFFFF">
+<?php
+if($pat>0){
+?>
+<td><?php echo "El $pat% que creció el patrimonio de la empresa respecto al período anteriror, puede ser un indicio de que la empresa está generando beneficios y creando valor para sus accionistas o bien que esta emitiendo nuevas acciones.
+";
+?></td>
+<?php
+}elseif($pat<0){
+?>
+<td><?php echo "La disminución del patrimonio en un $pat% implica una reducción en los fondos propios de la empresa, lo que implicaria que se uso capital social para hacer frente a las obligaciones de la empresa.
+";
+?></td>
+<?php
+}
+?>
 </tr>
 </table>
       
---
+++ resources/views/analisis/show11.blade.php
@@ -0,0 +1,106 @@
+@extends('template.plantilla2')
+@section('content')
+
+<?php 
+
+$conexion=mysqli_connect('localhost','root','','sistema_academico');
+
+?>
+
+<h1>Analisis Horizontal</h1>
+<table class="table table-bordered thead-dark table-hover table-sm" id="demo">
+
+
+ <tr>
+ <th>Cuenta</th>
+ <th>Valor Anterior</th>
+ <th>Valor Actual</th>
+ <th>Variación Absoluta</th>
+ <th>Variación relativa</th>
+</tr>
+ </thead>
+<tbody>
+
+
+<?php
+$valor1='2020-10-01';
+$valor2='2020-11-30';
+ $sql = "SELECT c.nombre AS nom, c.monto AS valor_Actual, b.monto As valor_anterior, c.monto-b.monto AS variacion From resultados c, resultados b WHERE c.fecha_final='$valor2' AND b.fecha_inicio='$valor1' AND c.nombre=b.nombre";
+$result=mysqli_query($conexion,$sql);
+while($mostrar=mysqli_fetch_array($result)){
+$a=$mostrar['valor_anterior'];
+$b=$mostrar['variacion'];
+$c=$b/$a;
+$d=$c*100;
+$e=round($d,2);
+?>
+
+<tr>
+<td><?php echo $mostrar['nom']?></td>
+<td><?php echo $mostrar['valor_anterior']?></td>
+<td><?php echo $mostrar['valor_Actual']?></td>
+<td><?php echo $mostrar['variacion']?></td>
+<td><?php echo $e?>%</td>
+
+<?php
+if($mostrar['nom']=='UTILIDAD BRUTA'){
+$ut= $e;
+}
+if($mostrar['nom']=='UTILIDAD ANTES DE PART E IMP'){
+      $uti= $e;
+      }
+
+
+
+?>
+
+</tr>
+
+<?php
+}
+?>
+
+</tbody>
+
+    
+</table>
+<h2>Resultados</h2>
+<table width="100%" border="0" cellspacing="0" cellpadding="4">
+<tr bgcolor="#FFFFFF">
+<?php
+if($ut>0){
+?>
+<td><?php echo "";
+?></td>
+<?php
+}elseif($ut==0){
+?>
+<td><?php echo "";
+?></td>
+<?php
+}else{
+?>
+<td><?php echo "";
+?></td>
+<?php
+}
+?>
+</tr>
+<tr bgcolor="#FFFFFF">
+<?php
+if($uti>0){
+?>
+<td><?php echo " ";
+?></td>
+<?php
+}elseif($uti<0){
+?>
+<td><?php echo "";
+?></td>
+<?php
+}
+?>
+</tr>
+
+</table>
+@endsection
