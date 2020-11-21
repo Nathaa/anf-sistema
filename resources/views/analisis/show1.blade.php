884ca114a922a92d22e3358b2da8303a31c6c2be
Eloisa-E <65208993+Eloisa-E@users.noreply.github.com>
2020-11-20 21:46:31 -0600

    AnaHorFinal

--- resources/views/analisis/show1.blade.php
+++ resources/views/analisis/show1.blade.php
@@ -3,7 +3,7 @@
 <?php 
 $valor1=$valant;
 $valor2=$valact;
-$conexion=mysqli_connect('localhost:33065','root','','analisis');
+$conexion=mysqli_connect('localhost','root','','sistema_academico');
 
 ?>
 
@@ -37,13 +37,23 @@ $e=round($d,2);
 ?>
 
 <tr>
+<?php if($mostrar['nom'] == "ACTIVO" || $mostrar['nom'] == "ACTIVO CORRIENTE" || $mostrar['nom'] == "ACTIVO NO CORRIENTE"
+                   || $mostrar['nom'] == "PASIVO" || $mostrar['nom'] == "PASIVO CORRIENTE" || $mostrar['nom'] == "PASIVO NO CORRIENTE"
+                   || $mostrar['nom'] == "PATRIMONIO"){ ?>
+<td style="font-weight:bold;"><?php echo $mostrar['nom']?></td>
+<td style="font-weight:bold;"><?php echo $mostrar['valor_anterior']?></td>
+<td style="font-weight:bold;"><?php echo $mostrar['valor_Actual']?></td>
+<td style="font-weight:bold;"><?php echo $mostrar['variacion']?></td>
+<td style="font-weight:bold;"><?php echo $e?>%</td>
+              <?php }else{ ?>
+              
 <td><?php echo $mostrar['nom']?></td>
 <td><?php echo $mostrar['valor_anterior']?></td>
 <td><?php echo $mostrar['valor_Actual']?></td>
 <td><?php echo $mostrar['variacion']?></td>
 <td><?php echo $e?>%</td>
-
 <?php
+              }
 if($mostrar['nom']=='ACTIVO'){
 $act= $e;
 }
@@ -73,7 +83,7 @@ if($mostrar['nom']=='PATRIMONIO'){
 <?php
 if($act>0){
 ?>
-<td><?php echo "La empresa logro un crecimiento  de $act de un período a otro, lo cual puede indicar que hubo aumento en las ventas o que los cambios realizados para la ejeucion de estas fueron acertivos.";
+<td><?php echo "La empresa logro un crecimiento  de <strong>$act%</strong> de un período a otro, lo cual puede indicar que hubo aumento en las ventas o que los cambios realizados para la ejeucion de estas fueron acertivos.";
 ?></td>
 <?php
 }elseif($act==0){
@@ -83,7 +93,7 @@ if($act>0){
 <?php
 }else{
 ?>
-<td><?php echo "La disminución del activo de la empresa en un $act%,implica que la empresa esta teniendo pérdidas duarante su ejercicio, por lo que se recomienda que revalue sus politicas de ventas y cobranzas para el siguiente periodo";
+<td><?php echo "La disminución del activo de la empresa en un <strong>$act%</strong>,implica que la empresa esta teniendo pérdidas duarante su ejercicio, por lo que se recomienda que revalue sus politicas de ventas y cobranzas para el siguiente periodo";
 ?></td>
 <?php
 }
@@ -93,13 +103,13 @@ if($act>0){
 <?php
 if($pas>0){
 ?>
-<td><?php echo "El aumento de $pas% en los  pasivo indica que la empresa se encuentra en una buena posición económica y que  puede disponer de credito adicional , por parte de los acreedores.
+<td><?php echo "El aumento de <strong>$pas%</strong>en los  pasivo indica que la empresa se encuentra en una buena posición económica y que  puede disponer de credito adicional , por parte de los acreedores.
 ";
 ?></td>
 <?php
 }elseif($pas<0){
 ?>
-<td><?php echo "La empresa decreció un $pas% de un período a otro, la disminución en los pasivos implica que se estan utilizando fondos para amortizar o cancelar cuentas de los acreedores.
+<td><?php echo "La empresa decreció un <strong>$pas%</strong> de un período a otro, la disminución en los pasivos implica que se estan utilizando fondos para amortizar o cancelar cuentas de los acreedores.
 ";
 ?></td>
 <?php
@@ -110,13 +120,13 @@ if($pas>0){
 <?php
 if($pat>0){
 ?>
-<td><?php echo "El $pat% que creció el patrimonio de la empresa respecto al período anteriror, puede ser un indicio de que la empresa está generando beneficios y creando valor para sus accionistas o bien que esta emitiendo nuevas acciones.
+<td><?php echo "El <strong>$pat%</strong> que creció el patrimonio de la empresa respecto al período anteriror, puede ser un indicio de que la empresa está generando beneficios y creando valor para sus accionistas o bien que esta emitiendo nuevas acciones.
 ";
 ?></td>
 <?php
 }elseif($pat<0){
 ?>
-<td><?php echo "La disminución del patrimonio en un $pat% implica una reducción en los fondos propios de la empresa, lo que implicaria que se uso capital social para hacer frente a las obligaciones de la empresa.
+<td><?php echo "La disminución del patrimonio en un <strong>$pat%</strong> implica una reducción en los fondos propios de la empresa, lo que implicaria que se uso capital social para hacer frente a las obligaciones de la empresa.
 ";
 ?></td>
 <?php
--- resources/views/analisis/show11.blade.php
+++ resources/views/analisis/show11.blade.php
@@ -36,6 +36,14 @@ $e=round($d,2);
 ?>
 
 <tr>
+<?php if($mostrar['nom'] == "UTILIDAD BRUTA" || $mostrar['nom'] == "UTILIDAD DE OPERACION" || $mostrar['nom'] == "UTILIDADES ANTES DE PART E IMP"
+                   || $mostrar['nom'] == "UTILIDAD (PERDIDA) NETA"){ ?>
+<td style="font-weight:bold;"><?php echo $mostrar['nom']?></td>
+<td style="font-weight:bold;"><?php echo $mostrar['valor_anterior']?></td>
+<td style="font-weight:bold;"><?php echo $mostrar['valor_Actual']?></td>
+<td style="font-weight:bold;"><?php echo $mostrar['variacion']?></td>
+<td style="font-weight:bold;"><?php echo $e?>%</td>
+              <?php }else{ ?>
 <td><?php echo $mostrar['nom']?></td>
 <td><?php echo $mostrar['valor_anterior']?></td>
 <td><?php echo $mostrar['valor_Actual']?></td>
@@ -43,6 +51,7 @@ $e=round($d,2);
 <td><?php echo $e?>%</td>
 
 <?php
+              }
 if($mostrar['nom']=='UTILIDAD BRUTA'){
 $ut= $e;
 }
@@ -71,14 +80,14 @@ if($mostrar['nom']=='VENTAS NETAS'){
       if($ut>0){
 ?>
 <td><?php 
-echo "La empresa tuvo un incremento favorable de $ut% en la utilidad bruta,lo cual indica que los cambios realizados en
+echo "La empresa tuvo un incremento favorable de <strong>$ut%</strong> en la utilidad bruta,lo cual indica que los cambios realizados en
 los precios de venta, los volumens de produccion  y los costos de compra han sido acertivos.";
 ?></td>
 <?php
       }elseif($ut<0){
 ?>
 <td><?php
-echo "La empresa tuvo un decrecimiento del $ut% respecto al periodo anteior, por lo que se recomienda evaluar los precios y el volumen de ventas
+echo "La empresa tuvo un decrecimiento del <strong>$ut%</strong> respecto al periodo anteior, por lo que se recomienda evaluar los precios y el volumen de ventas
 para solventar el problema. ";
 ?></td>
 <?php
@@ -95,7 +104,7 @@ echo "El aumento en los gastos de operación no se refleja en las ventas obtenid
 }elseif($utop>0 && $vn>0 && $utop<$vn){
 ?>
  <td><?php
- echo "El incremento de $utop% en los gastos operativos fue acertivo, pues las ventas incrementaron un $vn%";
+ echo "El incremento de <strong>$utop%</strong> en los gastos operativos fue acertivo, pues las ventas incrementaron un <strong>$vn%</strong>";
  ?></td>
  <?php
 }?></tr>
